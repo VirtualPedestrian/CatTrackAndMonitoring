2e67e7b3cf8493f63c768a76f040ac7381d90e29
VirtualPedestrian <73787177+VirtualPedestrian@users.noreply.github.com>
2026-04-30 06:01:20 +0800

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/cattletrackingmonitor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cattletrackingmonitor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/aiman/Downloads/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/aiman/StudioProjects/cattletrackingmonitor" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/aiman/Downloads/flutter"
+  "PROJECT_DIR=/home/aiman/StudioProjects/cattletrackingmonitor"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuMzguNg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049OGI4NzI4Njg0OQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NzhmYzMwMTJlNA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC43"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/aiman/StudioProjects/cattletrackingmonitor/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/aiman/StudioProjects/cattletrackingmonitor/lib/main.dart"
+)
